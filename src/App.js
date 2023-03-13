b827cea0a58b3f40b49046a96db7bec0d5af7d7d
abubakar528 <muhammadabubakar7526@gmail.com>
2023-03-13 19:52:56 +0500

    wallet connectd with wegmi library

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { arbitrum, mainnet, polygon } from 'wagmi/chains'
+import { Web3Button } from '@web3modal/react'
+
+const chains = [arbitrum, mainnet, polygon]
+const projectId = '0b229217d127d7509bbe20c087c66b8f'
+const { provider } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  provider
+})
+const ethereumClient = new EthereumClient(wagmiClient, chains)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <WagmiConfig client={wagmiClient}>
+        <Web3Button />
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
       </header>
     </div>
   );
